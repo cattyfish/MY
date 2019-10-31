a40e439bd38e3a0beb3784af68d01c0ff7c3e351
MMY <939268145@qq.com>
2019-10-31 14:32:18 +0800

    Update InsertionSort.cpp

--- Algorithm/Sort/InsertionSort.cpp
+++ Algorithm/Sort/InsertionSort.cpp
@@ -11,16 +11,16 @@ using namespace std;
 
 void InsertionSort(int * array,int length)
 {
-    for(int i=1;i<length;i++)
+    for(int i=1;i<length;i++)//假定第一个数字是有序的，从第二个数字开始插入有序数组，所以起始索引是1
     {
         int preIndex=i-1;
-        int current=array[i];
+        int current=array[i];//这里要用一个变量记录下要插入有序数组的值，因为下面的操作可能会更改array[i]的值
         while(preIndex>=0 && array[preIndex]>current)
         {
             array[preIndex+1]=array[preIndex];
             preIndex--;
         }
-        array[preIndex+1]=current;
+        array[preIndex+1]=current;//这里的所以是preIndex+1,不清楚就用2，1举例
     }
 }
 
